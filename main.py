dfa2c1170a485bc5983799823014fe5f7c732764
Louis Tamagny <louis.tamagny@gmail.com>
2024-07-03 11:33:30 +0200

    add quizz, question and answer post routes

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def get_session():
     with Session(engine) as session:
         yield session
 
+
+
 @app.get("/quizzes/{quizz_id}", response_model=QuizzPublicWithQuestions)
 async def read_quizz(*, session: Session = Depends(get_session), quizz_id):
     return session.get(Quizz, quizz_id)
@@ -18,3 +20,29 @@ async def read_quizz(*, session: Session = Depends(get_session), quizz_id):
 @app.get("/quizzes", response_model=list[QuizzPublicWithQuestions])
 async def read_quizz(*, session: Session = Depends(get_session)):
     return session.exec(select(Quizz)).all()
+
+@app.post("/quizzes", response_model=QuizzPublicWithQuestions)
+async def create_quizz(quizz: Quizz, session: Session = Depends(get_session)):
+    session.add(quizz)
+    session.commit()
+    session.refresh(quizz)
+    return quizz
+
+@app.post("/quizzes/{quizz_id}/questions", response_model=QuizzPublicWithQuestions)
+async def create_question(question: Question, quizz_id: int, session: Session= Depends(get_session)):
+    quizz = session.get(Quizz, quizz_id)
+    question.quizz = quizz
+    session.add(question)
+    session.commit()
+    session.refresh(quizz)
+    return quizz
+
+@app.post("/questions/{question_id}/answers", response_model=QuestionPublicWithAnswers)
+async def create_answers(answers: list[Answer], question_id: int, session: Session= Depends(get_session)):
+    q = session.get(Question, question_id)
+    for a in answers:
+        a.question = q
+        session.add(a)
+    session.commit()
+    session.refresh(q)
+    return q
